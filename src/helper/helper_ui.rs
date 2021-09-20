e57b95fd2169995e6a43cd2a604048bb55e74a46
y1z <whyis2cool@gmail.com>
2021-09-20 01:58:13 -0500

    Add utility function's for calculating the size of elements

---
+++ src/helper/helper_ui.rs
@@ -0,0 +1,24 @@
+/// how much of a given size fit in a percentage
+///
+/// EXAMPLE
+/// ```
+/// let res = calculate_size_of_percentage(0.5,100); // should be 50
+/// assert_eq!(res,50);
+/// ```
+pub fn calculate_size_of_percentage(percentage_of_size: f32, size_of_element: u32) -> u32 {
+  assert!(
+    percentage_of_size < 1.0000001f32 && 0.000000f32 <= percentage_of_size,
+    "desired percentage is out of range"
+  );
+  (percentage_of_size * size_of_element as f32) as u32
+}
+
+/// Does the same as *[calculate_size_of_percentage]* this is just here for code clarity
+pub fn calculate_ui_element_width(percentage_of_width: f32, ui_width: u32) -> u32 {
+  calculate_size_of_percentage(percentage_of_width, ui_width)
+}
+
+/// Does the same as *[calculate_size_of_percentage]* this is just here for code clarity
+pub fn calculate_ui_element_height(percentage_of_height: f32, ui_height: u32) -> u32 {
+  calculate_size_of_percentage(percentage_of_height, ui_height)
+}
