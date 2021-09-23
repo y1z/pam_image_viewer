8258130855cda4e2a250e79ad3e22232a8339354
y1z <whyis2cool@gmail.com>
2021-09-22 19:32:59 -0500

    Add functions for parsing a '.ppm'
    The reason for this is because the '.ppm' format is more simple for me to parse and will use some of this experience for parsing the .pam format

--- src/helper/helper_read_file.rs
+++ src/helper/helper_read_file.rs
@@ -12,12 +12,21 @@ pub struct UnsignedNumberParseResult {
   ending_index: usize,
 }
 
-pub fn convert_ppm_file_to_pixel_buffer(path_to_file: String) -> Vec<Color> {
+#[derive(Debug)]
+pub struct PixelBuffer {
+  pub buffer: Vec<Color>,
+  pub width: u32,
+  pub height: u32,
+}
+
+pub fn convert_ppm_file_to_pixel_buffer(path_to_file: String) -> PixelBuffer {
   let file = File::open(&path_to_file); //op(path_to_file);
   let can_open_file = path_to_file.ends_with(PPM_FORMAT_EXTENTION) && file.is_ok();
   let mut buffer: Vec<u8> = vec![];
 
   let mut result: Vec<Color> = vec![];
+  let mut result_width: u32 = 0;
+  let mut result_height: u32 = 0;
 
   if can_open_file {
     let mut reader = BufReader::new(file.unwrap());
@@ -26,11 +35,23 @@ pub fn convert_ppm_file_to_pixel_buffer(path_to_file: String) -> Vec<Color> {
     if has_read_successfully && buffer[0] == 'P' as u8 && buffer[1] == '6' as u8 {
       let buffer_size = buffer.len();
       let mut buffer_index = 2;
+
       let width_height_maxval = parse_ppm_width_height_maxval(&buffer, &mut buffer_index);
 
       println!(
         "width : {}\nheight : {}\nmaxval : {}",
         width_height_maxval[0], width_height_maxval[1], width_height_maxval[2]
+      );
+
+      result_width = width_height_maxval[0];
+      result_height = width_height_maxval[1];
+
+      result = parse_RGB_data(
+        result_width,
+        result_height,
+        width_height_maxval[2],
+        &mut buffer,
+        &mut buffer_index,
       );
     }
   } else {
@@ -40,7 +61,11 @@ pub fn convert_ppm_file_to_pixel_buffer(path_to_file: String) -> Vec<Color> {
     );
   }
 
-  result
+  PixelBuffer {
+    buffer: result,
+    width: result_width,
+    height: result_height,
+  }
 }
 
 fn parse_ppm_width_height_maxval(container: &Vec<u8>, index: &mut usize) -> [u32; 3] {
@@ -57,6 +82,8 @@ fn parse_ppm_width_height_maxval(container: &Vec<u8>, index: &mut usize) -> [u32
       *index += 1;
     }
   }
+  // skip empty space
+  *index += 1;
 
   result
 }
@@ -76,7 +103,7 @@ pub fn parse_unsigned_numbers(container: &Vec<u8>, index: &mut usize) -> Unsigne
     if !continue_parsing {
       break 'git_digits;
     } else {
-      let value_of_digit = (current_value - '0' as u8);
+      let value_of_digit = current_value - '0' as u8;
       individual_digits[digit_index] = value_of_digit;
       digit_index += 1;
     }
@@ -96,4 +123,34 @@ pub fn parse_unsigned_numbers(container: &Vec<u8>, index: &mut usize) -> Unsigne
     starting_index: starting_index_,
     ending_index: *index,
   }
+}
+
+fn parse_RGB_data(
+  width: u32,
+  height: u32,
+  max_val: u32,
+  container: &Vec<u8>,
+  index: &mut usize,
+) -> Vec<Color> {
+  let size_of_buffer = width as usize * height as usize;
+  let mut result: Vec<Color> = Vec::with_capacity(size_of_buffer);
+  let starting_value = *index as usize;
+  let mut rgb_index: usize = 0;
+  let mut current_color: Color = Color::WHITE;
+  for x in starting_value..container.len() {
+    match rgb_index % 3 {
+      0 => current_color.r = container[x],
+      1 => current_color.g = container[x],
+      2 => {
+        current_color.b = container[x];
+        result.push(current_color);
+      }
+      _ => {}
+    }
+
+    *index += 1;
+    rgb_index += 1;
+  }
+
+  result
 }
