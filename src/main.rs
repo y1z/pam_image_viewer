bbbf562be17ebc2e075864ecfacb09d7d0e384b3
y1z <whyis2cool@gmail.com>
2021-10-02 03:59:47 -0500

    Can now filter the color of the given input.

--- src/main.rs
+++ src/main.rs
@@ -114,7 +114,10 @@ pub fn main_loop(
       pixel_buffer.buffer.as_slice(),
       (0, 0),
       (pixel_buffer.width, pixel_buffer.height),
-      render_mode::RenderMode::RGB_RENDER_MODE,
+      render_mode::RenderMode::from(
+        render_mode::RenderModeBitMasks::BLUE_ONLY as u32
+          | render_mode::RenderModeBitMasks::GREEN_ONLY as u32,
+      ),
     );
 
     for event in event_pump.poll_iter() {
