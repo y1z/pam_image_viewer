2d78569ae72b693e8daab5dd0eb4f351e3d49719
y1z <whyis2cool@gmail.com>
2021-10-08 02:10:56 -0500

    Moved parse functions to their own files

---
+++ src/helper/helper_parse_functions.rs
@@ -0,0 +1,212 @@
+use crate::helper_consts::*;
+use crate::helper_pam_header::*;
+use sdl2::pixels::Color;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
+
+#[derive(Debug, Clone, Copy)]
+pub struct UnsignedNumberParseResult {
+  number_parsed: Option<u32>,
+  starting_index: usize,
+  ending_index: usize,
+}
+//
+#[derive(Debug)]
+pub struct PixelBuffer {
+  pub buffer: Vec<Color>,
+  pub width: u32,
+  pub height: u32,
+}
+
+pub fn parse_ppm_width_height_maxval(container: &Vec<u8>, index: &mut usize) -> [u32; 3] {
+  let mut result = [0u32, 0u32, 0u32];
+
+  for x in 0..result.len() {
+    'find_numbers: loop {
+      if container[*index].is_ascii_digit() {
+        let parsing_result = parse_unsigned_numbers(container, index);
+        result[x] = parsing_result.number_parsed.unwrap();
+        break 'find_numbers;
+      }
+      *index += 1;
+    }
+  }
+  // skip empty space
+  *index += 1;
+
+  result
+}
+
+pub fn is_valid_ppm_header(container: &[u8]) -> bool {
+  let has_magic_number = container[0] == 'P' as u8 && container[1] == '6' as u8;
+  has_magic_number
+}
+
+// &[u8]
+pub fn parse_unsigned_numbers(container: &[u8], index: &mut usize) -> UnsignedNumberParseResult {
+  let starting_index_ = *index;
+
+  let mut individual_digits: [u8; 10] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  let mut digit_index: usize = 0;
+
+  'git_digits: loop {
+    let current_value = container[*index];
+    let continue_parsing = current_value.is_ascii_digit();
+    if !continue_parsing {
+      break 'git_digits;
+    } else {
+      let value_of_digit = current_value - '0' as u8;
+      individual_digits[digit_index] = value_of_digit;
+      digit_index += 1;
+    }
+
+    *index += 1usize;
+  }
+
+  let mut resulting_value: u32 = 0;
+  let mut mul_digit: u32 = 1;
+  for x in (0..digit_index).rev() {
+    resulting_value = resulting_value + (individual_digits[x] as u32 * mul_digit);
+    mul_digit *= 10;
+  }
+
+  UnsignedNumberParseResult {
+    number_parsed: Some(resulting_value),
+    starting_index: starting_index_,
+    ending_index: *index,
+  }
+}
+
+pub fn parse_unsigned_numbers_string(
+  container: &String,
+  index: &mut usize,
+) -> UnsignedNumberParseResult {
+  parse_unsigned_numbers(container.as_bytes(), index)
+}
+
+pub fn parse_unsigned_numbers_vec_u8(
+  container: &Vec<u8>,
+  index: &mut usize,
+) -> UnsignedNumberParseResult {
+  return parse_unsigned_numbers(container, index);
+}
+
+pub fn parse_ppm_data(buffer: &Vec<u8>) -> PixelBuffer {
+  let mut buffer_index = 2;
+  let width_height_maxval = parse_ppm_width_height_maxval(buffer, &mut buffer_index);
+
+  println!(
+    "width : {}\nheight : {}\nmaxval : {}",
+    width_height_maxval[0], width_height_maxval[1], width_height_maxval[2]
+  );
+
+  let result_width = width_height_maxval[0];
+  let result_height = width_height_maxval[1];
+
+  let result = parse_ppm_RGB_data(
+    result_width,
+    result_height,
+    width_height_maxval[2],
+    &buffer,
+    &mut buffer_index,
+  );
+
+  PixelBuffer {
+    buffer: result,
+    width: result_width,
+    height: result_height,
+  }
+}
+
+fn parse_ppm_RGB_data(
+  width: u32,
+  height: u32,
+  max_val: u32,
+  container: &Vec<u8>,
+  index: &mut usize,
+) -> Vec<Color> {
+  let size_of_buffer = width as usize * height as usize;
+  let mut result: Vec<Color> = Vec::with_capacity(size_of_buffer);
+  let starting_value = *index as usize;
+  let mut rgb_index: usize = 0;
+  let mut current_color: Color = Color::WHITE;
+  for x in starting_value..container.len() {
+    match rgb_index % 3 {
+      0 => current_color.r = container[x],
+      1 => current_color.g = container[x],
+      2 => {
+        current_color.b = container[x];
+        result.push(current_color);
+      }
+      _ => {}
+    }
+
+    *index += 1;
+    rgb_index += 1;
+  }
+
+  result
+}
+
+/// extracts the data of the pam header : http://netpbm.sourceforge.net/doc/pam.html#layout
+/// It's assumed to be at the start of the .pam file
+pub fn parse_pam_header(buffer: &String) -> Option<PamHeader> {
+  // string and the mothod needed to parse them
+  let strs_methods = &PAM_HEADER_EXPECTED_STRS_METHODS;
+  // keeps track of which string was already parsed
+  let mut found_and_parsed_strings: [(bool, crate::helper_pam_header::StringAndMethod); 7] = [
+    (false, strs_methods[0]),
+    (false, strs_methods[1]),
+    (false, strs_methods[2]),
+    (false, strs_methods[3]),
+    (false, strs_methods[4]),
+    (false, strs_methods[5]),
+    (false, strs_methods[6]),
+  ];
+
+  let mut width_res: u32 = 1;
+  let mut height_res: u32 = 1;
+  let mut depth_res: u32 = 1;
+  let mut max_val_res: u16 = 1;
+  let mut tuple_type_res: TupleTypes = TupleTypes::UNDEFINED;
+  for line in buffer.lines() {
+    for string in found_and_parsed_strings.iter_mut() {
+      let string_and_method = string.1;
+      let find_string = line.find(string_and_method.string);
+      if !string.0 && find_string.is_some() {
+        string.0 = true;
+        match string_and_method.parse_method {
+          ParseOrFindMethod::FIND_START => {}
+          ParseOrFindMethod::FIND_END => {}
+          ParseOrFindMethod::PARSE_STRING => {}
+          ParseOrFindMethod::PARSE_NUMBER => {
+            if let Some(digit_index) = line.find(|c: char| c.is_ascii_digit()) {
+              let mut mut_digit_index = digit_index;
+              parse_unsigned_numbers(line.as_bytes(), &mut mut_digit_index);
+            }
+          }
+        }
+      }
+    }
+  }
+
+  /// check that each of the necesary strings where found
+  for index in 0..found_and_parsed_strings.len() {
+    if !found_and_parsed_strings[index].0 {
+      eprintln!(
+        "Pam image missing the [{}] in the header",
+        found_and_parsed_strings[index].1.string
+      );
+      return None;
+    }
+  }
+
+  Some(PamHeader {
+    height: height_res,
+    width: width_res,
+    depth: depth_res,
+    max_val: max_val_res,
+    tuple_types: tuple_type_res,
+  })
+}
--- src/helper/helper_read_file.rs
+++ src/helper/helper_read_file.rs
@@ -1,24 +1,11 @@
+use crate::helper_consts::*;
+use crate::helper_parse_functions::*;
 use sdl2::pixels::Color;
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
 
-const PPM_FORMAT_EXTENTION: &'static str = ".ppm";
-
-pub struct UnsignedNumberParseResult {
-  number_parsed: Option<u32>,
-  starting_index: usize,
-  ending_index: usize,
-}
-
-#[derive(Debug)]
-pub struct PixelBuffer {
-  pub buffer: Vec<Color>,
-  pub width: u32,
-  pub height: u32,
-}
-
-pub fn convert_ppm_file_to_pixel_buffer(path_to_file: String) -> PixelBuffer {
+pub fn convert_ppm_file_to_pixel_buffer(path_to_file: String) -> Option<PixelBuffer> {
   let file = File::open(&path_to_file); //op(path_to_file);
   let can_open_file = path_to_file.ends_with(PPM_FORMAT_EXTENTION) && file.is_ok();
   let mut buffer: Vec<u8> = vec![];
@@ -27,30 +14,14 @@ pub fn convert_ppm_file_to_pixel_buffer(path_to_file: String) -> PixelBuffer {
   let mut result_width: u32 = 0;
   let mut result_height: u32 = 0;
 
+  let mut result: PixelBuffer;
+
   if can_open_file {
     let mut reader = BufReader::new(file.unwrap());
     let has_read_successfully = reader.read_to_end(&mut buffer).is_ok();
 
-    if has_read_successfully && buffer[0] == 'P' as u8 && buffer[1] == '6' as u8 {
-      let mut buffer_index = 2;
-
-      let width_height_maxval = parse_ppm_width_height_maxval(&buffer, &mut buffer_index);
-
-      println!(
-        "width : {}\nheight : {}\nmaxval : {}",
-        width_height_maxval[0], width_height_maxval[1], width_height_maxval[2]
-      );
-
-      result_width = width_height_maxval[0];
-      result_height = width_height_maxval[1];
-
-      result = parse_ppm_RGB_data(
-        result_width,
-        result_height,
-        width_height_maxval[2],
-        &mut buffer,
-        &mut buffer_index,
-      );
+    if has_read_successfully && is_valid_ppm_header(&buffer) {
+      return Some(parse_ppm_data(&buffer));
     }
   } else {
     panic!(
@@ -59,92 +30,39 @@ pub fn convert_ppm_file_to_pixel_buffer(path_to_file: String) -> PixelBuffer {
     );
   }
 
-  PixelBuffer {
+  None
+}
+
+pub fn convert_pam_file_to_pixel_buffer(path_to_file: String) -> Option<PixelBuffer> {
+  let file = File::open(&path_to_file);
+  let can_open_file = path_to_file.ends_with(PAM_FORMAT_EXTENTION) && file.is_ok();
+  let mut buffer: Vec<u8> = vec![];
+
+  let mut result: Vec<Color> = vec![];
+  let mut result_width: u32 = 0;
+  let mut result_height: u32 = 0;
+
+  if can_open_file {
+    let mut reader = BufReader::new(file.unwrap());
+    let mut buffer: String = String::new();
+    let reader_result = reader.read_to_string(&mut buffer);
+    if reader_result.is_err() {
+      panic!(
+        "Error from reading [{}] : {} ",
+        path_to_file,
+        reader_result.unwrap_err()
+      );
+    }
+
+    let pam_header = parse_pam_header(&path_to_file);
+  } else {
+    eprintln!("cannot open file {}", path_to_file);
+    return None;
+  }
+
+  Some(PixelBuffer {
     buffer: result,
     width: result_width,
     height: result_height,
-  }
-}
-
-fn parse_ppm_width_height_maxval(container: &Vec<u8>, index: &mut usize) -> [u32; 3] {
-  let mut result = [0u32, 0u32, 0u32];
-
-  for x in 0..result.len() {
-    'find_numbers: loop {
-      if container[*index].is_ascii_digit() {
-        let parsing_result = parse_unsigned_numbers(container, index);
-        result[x] = parsing_result.number_parsed.unwrap();
-        break 'find_numbers;
-      }
-      *index += 1;
-    }
-  }
-  // skip empty space
-  *index += 1;
-
-  result
-}
-
-pub fn parse_unsigned_numbers(container: &Vec<u8>, index: &mut usize) -> UnsignedNumberParseResult {
-  let starting_index_ = *index;
-
-  let mut individual_digits: [u8; 10] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-  let mut digit_index: usize = 0;
-
-  'git_digits: loop {
-    let current_value = container[*index];
-    let continue_parsing = current_value.is_ascii_digit();
-    if !continue_parsing {
-      break 'git_digits;
-    } else {
-      let value_of_digit = current_value - '0' as u8;
-      individual_digits[digit_index] = value_of_digit;
-      digit_index += 1;
-    }
-
-    *index += 1usize;
-  }
-
-  let mut resulting_value: u32 = 0;
-  let mut mul_digit: u32 = 1;
-  for x in (0..digit_index).rev() {
-    resulting_value = resulting_value + (individual_digits[x] as u32 * mul_digit);
-    mul_digit *= 10;
-  }
-
-  UnsignedNumberParseResult {
-    number_parsed: Some(resulting_value),
-    starting_index: starting_index_,
-    ending_index: *index,
-  }
-}
-
-fn parse_ppm_RGB_data(
-  width: u32,
-  height: u32,
-  max_val: u32,
-  container: &Vec<u8>,
-  index: &mut usize,
-) -> Vec<Color> {
-  let size_of_buffer = width as usize * height as usize;
-  let mut result: Vec<Color> = Vec::with_capacity(size_of_buffer);
-  let starting_value = *index as usize;
-  let mut rgb_index: usize = 0;
-  let mut current_color: Color = Color::WHITE;
-  for x in starting_value..container.len() {
-    match rgb_index % 3 {
-      0 => current_color.r = container[x],
-      1 => current_color.g = container[x],
-      2 => {
-        current_color.b = container[x];
-        result.push(current_color);
-      }
-      _ => {}
-    }
-
-    *index += 1;
-    rgb_index += 1;
-  }
-
-  result
+  })
 }
